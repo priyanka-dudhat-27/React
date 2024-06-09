15ec6d577ec281b87f6bede19d00eefa6c39b0bc
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-06-09 15:34:00 +0530

    brand-page(figma to react)

---
+++ project-1/brand-page(figma to react)/src/pages/Brand.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Hero from '../components/Hero'
+import Navbar from '../components/Navbar'
+
+const Brand = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Hero/>
+    </div>
+  )
+}
+
+export default Brand
